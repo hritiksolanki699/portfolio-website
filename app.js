4fbe96edc68e83db9357ca5463b8859296e77e50
Hritik <hritiksolanki699@gmail.com>
2022-06-23 10:58:32 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const humburger = document.querySelector("#hamburger")
+const menu = document.querySelector("#menu")
+const moon = document.querySelector("#moon")
+const body = document.querySelector("body")
+const hLink = document.querySelectorAll("#hLink")
+
+hamburger.addEventListener("click", () => {
+    menu.classList.toggle("hidden")
+    humburger.classList.toggle("bg-white")
+})
+
+hLink.forEach(link => {
+    link.addEventListener("click", () => {
+        menu.classList.toggle("hidden")
+        humburger.classList.toggle("bg-white")
+    })
+})
+
+moon.addEventListener("click", () => {
+    body.classList.toggle("dark")
+    
+})
